3800176838694abaf4313e3469f746437459baab
Karan Thukral <karanthukral@gmail.com>
2016-03-07 10:30:15 -0500

    Fixed formatting in brew.sh

--- provision/brew.sh
+++ provision/brew.sh
@@ -15,9 +15,7 @@ brew tap caskroom/cask 1>/dev/null
 brew tap caskroom/fonts 1>/dev/null
 
 # Install Brew Packages
-for pkg in brew-cask cmake fswatch gcc gnupg gnupg2 go imagemagick mercurial
-  mysql node phantomjs postgresql python redis wget battery autojump git
-  htop-osx tig tmux wifi-password; do
+for pkg in brew-cask cmake fswatch gcc gnupg gnupg2 go imagemagick mercurial mysql node phantomjs postgresql python redis wget autojump git htop-osx tig tmux wifi-password; do
   if brew list -1 | grep -q "^${pkg}\$"; then
     echo "Brew: Package '$pkg' is already installed"
   else
@@ -26,15 +24,13 @@ for pkg in brew-cask cmake fswatch gcc gnupg gnupg2 go imagemagick mercurial
   fi
 done
 
-# Install Brew Casks
-for pkg in 1password adium dropbox flux google-chrome google-drive
-  heroku-toolbelt java karabiner licecap sketch skype slack spotify steam
-  sublime-text vmware-fusion font-hack firefox alfred bartender font-octicons
-  iterm2 selfcontrol viscosity transmission; do
-  if brew cask info $pkg | grep -q "Not installed"; then
-    echo "Brew Cask: Installing '$pkg'"
-    brew cask install --appdir="/Applications" $pkg 1>/dev/null
-  else
-    echo "Brew Cask: Package '$pkg' is already installed"
-  fi
-done
+## Install Brew Casks
+#for pkg in 1password adium dropbox flux google-chrome google-drive heroku-toolbelt java licecap sketch skype slack spotify steam sublime-text vmware-fusion font-hack firefox alfred bartender font-octicons
+  #iterm2 selfcontrol viscosity transmission; do
+  #if brew cask info $pkg | grep -q "Not installed"; then
+    #echo "Brew Cask: Installing '$pkg'"
+    #brew cask install --appdir="/Applications" $pkg 1>/dev/null
+  #else
+    #echo "Brew Cask: Package '$pkg' is already installed"
+  #fi
+#done
